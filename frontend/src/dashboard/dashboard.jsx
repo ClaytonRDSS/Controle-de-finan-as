a914e3a1fad0da673ca2c97c37fcc489dec0d3da
Clayton Roberto Dos Santos <clayton.santos.s035@gmail.com>
2023-03-09 16:05:31 -0300

    create dashBoard

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React,{Component} from "react";
+import Row from "../common/layout/row";
 
+import Content from "../common/template/content";
+import ContentHeader from "../common/template/contentHeader";
+import ValueBox from "../common/widget/valueBox";
 
-export default props => (
-    <div>
-        <h1>DashBoard</h1>
-    </div>
-)
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Dashboard' small='Versão 1.0'/>
+                <Content>
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank' value='$10' text='Total de Créditos'/>
+                        <ValueBox cols='12 4' color='red' icon='credit-card' value='$10' text='Total de Débitos'/>
+                        <ValueBox cols='12 4' color='blue' icon='money' value='$0' text='Valor Consolidado'/>
+                    </Row>
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
