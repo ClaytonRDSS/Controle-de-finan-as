d4c61225ef2f3c51170c2848070e49c4c7c15178
Clayton Roberto Dos Santos <clayton.santos.s035@gmail.com>
2023-03-09 17:09:38 -0300

    create redux dashboard

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import reducers from './main/reducers'
 import App from './main/app'
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+
+const store = createStore(reducers)
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App /> 
+    </Provider>,
 document.getElementById('app'));
 
