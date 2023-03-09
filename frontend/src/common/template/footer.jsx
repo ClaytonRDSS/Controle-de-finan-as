7fd7392d90201f5da9dc163aa61036b007f4ca7c
Clayton Roberto Dos Santos <clayton.santos.s035@gmail.com>
2023-03-09 13:22:52 -0300

    create  routes, footer,

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2023
+            <a href="http://minhasFinanças.com.br" target='_blank'> - MinhasFinanças</a>.
+        </strong>
+    </footer>
+)
