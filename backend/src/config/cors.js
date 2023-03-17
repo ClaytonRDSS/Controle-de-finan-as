f3c640d05a2113678f7d757dd75c0360de48cb30
Clayton Roberto Dos Santos <clayton.santos.s035@gmail.com>
2023-03-17 18:32:35 -0300

    configuration headres

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -2,7 +2,8 @@
 //midllwer
 module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Credentials", "true");
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS, DELETE, PATCH');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept Authorization');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Request-Method, Access-Control-Request-Headers');
     next()
 }
