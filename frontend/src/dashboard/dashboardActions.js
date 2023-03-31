567807e5f2da5828bdeb88ab441b0a53c918f95b
Clayton Roberto Dos Santos <clayton.santos.s035@gmail.com>
2023-03-31 10:30:50 -0300

    route auteration

--- frontend/src/dashboard/dashboardActions.js
+++ frontend/src/dashboard/dashboardActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://127.0.0.1:3000/api'
+const BASE_URL = 'http://127.0.0.1:3003/api'
 
 export function getSummary() {
     const request = axios.get(`${BASE_URL}/billingCycles/summary`)
